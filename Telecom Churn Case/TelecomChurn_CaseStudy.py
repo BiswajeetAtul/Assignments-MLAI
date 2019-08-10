830e42a59b6247aa2f98d0f8f478bb337467643c
BiswajeetAtul <biswajeet.atul@live.com>
2019-08-11 01:09:00 +0530

    Updated the notebook till data preparation
    1. filtered the high valued customers
    2. tagged rows as churned and not churned
    3. removed churn phase attributes
    4. removed date attributes
    
    Next to do:
    EDA, Outlier Treatment, Multi-Collinearity treatment

--- Telecom Churn Case/TelecomChurn_CaseStudy.py
+++ Telecom Churn Case/TelecomChurn_CaseStudy.py
@@ -95,6 +95,148 @@ for col in list(teleData.columns):
         print("----------------------------------------------------------------------------------")
 
 
+# # Data Preperation
+
+# Finding the high values customers:
+# 1. Creating a new columns that has average of the revenues of 6th and 7th month
+# 2. Finding the 70th percentile and above.
+# 3. Filtering the data to get the High Valued customers
+# 
+
+# As we can see that all the Average Revenue Columns have no null values, we can proceed forward with finding the Average
+# 1. arpu_6        ---              99999 non-null float64
+# 2. arpu_7        ---              99999 non-null float64
+# 3. arpu_8        ---              99999 non-null float64
+# 4. arpu_9        ---              99999 non-null float64
+
+# In[10]:
+
+
+teleData['avg_6_7_revenue']=(teleData["arpu_6"]+teleData["arpu_7"])/2
+
+
+# In[12]:
+
+
+percentile70th= teleData['avg_6_7_revenue'].quantile(0.70)
+print("The 70th percentile average revenue is "+ str(percentile70th))
+
+
+# In[13]:
+
+
+teleData_HighValuesCustomers= teleData[teleData['avg_6_7_revenue']>=percentile70th]
+teleData_HighValuesCustomers.shape
+
+
+# Taking a look into the High Valued customers dataset:
+
+# In[16]:
+
+
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    display(teleData_HighValuesCustomers.head(10))
+
+
+# Now making all Null values 0.
+
+# In[18]:
+
+
+teleData_HighValuesCustomers_Treated=teleData_HighValuesCustomers.fillna(0)
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(teleData_HighValuesCustomers_Treated.info(verbose=True,null_counts =True))
+
+
+# We can see that all the null values have been treated.
+# Thus teleData_HighValuesCustomers_Treated is our dataset that contains the high valued customer with treated data.
+# The next step is to tag the numbers as churned(1) and not churned(0)based on the following columns:
+# 1. total_ic_mou_9
+# 2. total_og_mou_9
+# 3. vol_2g_mb_9
+# 4. vol_3g_mb_9
+
+# In[35]:
+
+
+filter=((teleData_HighValuesCustomers_Treated['total_ic_mou_9']==0)&(teleData_HighValuesCustomers_Treated['vol_2g_mb_9']==0)&(teleData_HighValuesCustomers_Treated['vol_3g_mb_9']==0)&(teleData_HighValuesCustomers_Treated['total_og_mou_9']==0))
+teleData_HighValuesCustomers_Treated['churned_tag']=np.where(filter, 1, 0)
+teleData_HighValuesCustomers_Treated['churned_tag'].sum()
+
+
+# In[26]:
+
+
+#filter=((teleData_HighValuesCustomers_Treated['total_ic_mou_9']==0)&(teleData_HighValuesCustomers_Treated['vol_2g_mb_9']==0)&(teleData_HighValuesCustomers_Treated['vol_3g_mb_9']==0)&(teleData_HighValuesCustomers_Treated['total_og_mou_9']==0))
+#tagged=teleData_HighValuesCustomers_Treated.ix[filter,list(teleData_HighValuesCustomers_Treated.columns)]
+#display(tagged.index)
+
+
+# In[33]:
+
+
+teleData_HighValuesCustomers_Treated.head(5)
+
+
+# Now we can see that the rows have a churned indicator against them.
+# We can delete all the columns that are related to the churn phase i.e. the '_9' columns
+# 
+# Removing all columns that have '_9' in them.
+
+# In[51]:
+
+
+print(len([x for x in teleData_HighValuesCustomers_Treated.columns if '_9' in x]))
+print(len(teleData_HighValuesCustomers_Treated.columns))
+teleData_HighValuesCustomers_Treated.shape
+
+
+# In[53]:
+
+
+colsToDrop=[x for x in teleData_HighValuesCustomers_Treated.columns if '_9' in x]
+
+
+# In[54]:
+
+
+teleData_HighValuesCustomers_Tagged=teleData_HighValuesCustomers_Treated.drop(colsToDrop,axis=1)
+
+
+# In[55]:
+
+
+teleData_HighValuesCustomers_Tagged.shape
+
+
+# Now we have removed all the columns that are related to the churn phase.
+# Lets have a look at the dataset now.
+
+# In[57]:
+
+
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    display(teleData_HighValuesCustomers_Tagged.head(5))
+
+
+# We might not need the date columns, so removing them as well.
+# 
+# creating a new dataset _naDates 
+
+# In[58]:
+
+
+teleData_HighValuesCustomers_naDates=teleData_HighValuesCustomers_Tagged.drop([x for x in teleData_HighValuesCustomers_Tagged.columns if 'date' in x],axis=1)
+teleData_HighValuesCustomers_naDates.shape
+
+
+# In[59]:
+
+
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    display(teleData_HighValuesCustomers_naDates.head(5))
+
+
 # In[ ]:
 
 
