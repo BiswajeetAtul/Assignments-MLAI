4c78c245beeae8d0b7f7608e321a9207f5e1dfa9
BiswajeetAtul <biswajeet.atul@live.com>
2019-10-14 20:59:01 +0530

    Created the Project Solution
    The Base Solution for Chatbot has been created.
    Working on the ZOMATO API Consumption file.

---
+++ ChatBot RASA/Chatbot_Ver_New/Chatbot_RestroSearch_Upgrad/ZomatoAPI.py
@@ -0,0 +1,9 @@
+import requests
+
+class ZomatoApi:
+    #Constructor
+    #Give your Api Key or use my key by default
+    def __init__(self,api_key='ab130b87323aa68eb2747b5fd1c1b188'):
+        self.api_key= api_key   
+
+    def 
